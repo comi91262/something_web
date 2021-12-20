4fc12268f73cd1e39b08b6e81b03607e971654e6
ykonomi <hgKaguyaToupako4er@gmail.com>
2021-12-20 11:52:26 +0900

    Fix a bug

--- db-world/migrations/000002_create_cities_table.down.sql
+++ db-world/migrations/000002_create_cities_table.down.sql
@@ -1,12 +1,1 @@
 DROP TABLE IF EXISTS `cities`;
-CREATE TABLE `cities` (
-  `ID` int NOT NULL AUTO_INCREMENT,
-  `Name` char(35) NOT NULL DEFAULT '',
-  `CountryCode` char(3) NOT NULL DEFAULT '',
-  `District` char(20) NOT NULL DEFAULT '',
-  `Population` int NOT NULL DEFAULT '0',
-  PRIMARY KEY (`ID`),
-  KEY `CountryCode` (`CountryCode`),
-  CONSTRAINT `city_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- db-world/migrations/000003_create_country_languages_table.down.sql
+++ db-world/migrations/000003_create_country_languages_table.down.sql
@@ -1,10 +1,1 @@
 DROP TABLE IF EXISTS `countrylanguages`;
-CREATE TABLE `countrylanguages` (
-  `CountryCode` char(3) NOT NULL DEFAULT '',
-  `Language` char(30) NOT NULL DEFAULT '',
-  `IsOfficial` enum('T','F') NOT NULL DEFAULT 'F',
-  `Percentage` decimal(4,1) NOT NULL DEFAULT '0.0',
-  PRIMARY KEY (`CountryCode`,`Language`),
-  KEY `CountryCode` (`CountryCode`),
-  CONSTRAINT `countryLanguage_ibfk_1` FOREIGN KEY (`CountryCode`) REFERENCES `country` (`Code`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
